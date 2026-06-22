e3f6130ec84e155a7e8d2af77513db9c83c65739
Kriva Lab <kriva.agency.dev@gmail.com>
2026-06-22 16:18:27 -0300

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,7 @@
-export { updateSession as middleware, config } from "./src/utils/supabase/middleware";
+export { updateSession as middleware } from "./src/utils/supabase/middleware";
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
