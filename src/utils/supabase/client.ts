cca45078f18c540eaa07f3ffbc7fcdef2eecac19
Kriva Lab <kriva.agency.dev@gmail.com>
2026-06-23 16:36:30 -0300

    fix: exclude mobile app from typescript build

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,8 +1,19 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () => {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+  console.log("SUPABASE_URL =", supabaseUrl);
+  console.log("SUPABASE_KEY EXISTS =", !!supabaseKey);
+
+  if (!supabaseUrl) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_URL missing");
+  }
+
+  if (!supabaseKey) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY missing");
+  }
 
   return createBrowserClient(supabaseUrl, supabaseKey);
 };
